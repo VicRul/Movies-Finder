a08d17882f9dcf42b2998ab0577d029d3a9adc27
victor <victor@victor-PC>
2020-09-15 16:22:24 +0700

    Корректировка импортов

--- MoviesFinder/src/main/java/org/vicrul/moviesfinder/api/MovieRepo.java
+++ MoviesFinder/src/main/java/org/vicrul/moviesfinder/api/MovieRepo.java
@@ -1,7 +1,6 @@
 package org.vicrul.moviesfinder.api;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 import org.vicrul.moviesfinder.api.queries.GetMovies;
 import org.vicrul.moviesfinder.domain.DetailsMovie;
